c3bb42dbad63bc4094fa2763297539c797776b18
rockymontallana <rockymontallana@gmail.com>
2025-11-26 00:49:24 +0800

    Initial commit

---
+++ src/main/java/com/localdev/product_crud_api/ProductCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.localdev.product_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localdev/product_crud_api/ProductCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localdev.product_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
